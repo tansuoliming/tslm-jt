ee91f16a37eef85ce67e15fb4d4c935810e870ca
tansuoliming <997044639@qq.com>
2021-04-09 19:52:14 +0800

    initial commit

---
+++ src/main/java/lm/com/vshop_parent/App.java
@@ -0,0 +1,13 @@
+package lm.com.vshop_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
